818541070f5a80194cea46648afbcc5ad731930f
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-09 16:20:47 +1000

    'Variable Annotations 08'

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Returns a function
+"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies its input by the given multiplier.
+
+    Args:
+        multiplier (float): The multiplier to apply.
+
+    Returns:
+        Callable[[float], float]: A function that takes a float and returns
+                                 the product as a float.
+    """
+    def multiplier_func(x: float) -> float:
+        return x * multiplier
+    return multiplier_func
