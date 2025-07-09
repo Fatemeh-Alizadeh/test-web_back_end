1c0d1a36ec133e7140c4b550c472232a83ce2c09
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-09 15:57:57 +1000

    'Variable Annotations 05'

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Calculate the sum of a list of floats.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Calculate the sum of a list of floats.
+
+    Args:
+        input_list (List[float]): A list of float numbers.
+
+    Returns:
+        float: The sum of all floats in the list.
+    """
+    return sum(input_list)
