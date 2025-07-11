793c5570a325f8932a7953de54d5dffd5fe5c80f
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-11 12:42:44 +1000

    'Async 02'

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Measure total runtime for async execution"""
+"""
+Measure total runtime for async execution
+"""
 import time
 import asyncio
 import importlib.util
@@ -7,7 +9,6 @@ import importlib.util
 spec = importlib.util.spec_from_file_location("custom_name", "./1-concurrent_coroutines.py")
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
-
 wait_n = module.wait_n
 
 
@@ -19,8 +20,3 @@ def measure_time(n: int, max_delay: int) -> float:
     total_time = end - start
 
     return total_time / n
-
-n = 5
-max_delay = 9
-
-print(measure_time(n, max_delay))
